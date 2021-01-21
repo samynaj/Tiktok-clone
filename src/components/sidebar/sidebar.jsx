a409aae27699426b208d10a35fd70e673eddae5e
samynaj <samuelnnj@gmail.com>
2021-01-21 23:23:32 +0100

    added side bar icons  and like funtionality

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import MessageIcon from '@material-ui/icons/Message';
+import ShareIcon from '@material-ui/icons/Share';
+import  "./sidebar.css"
 
 function Sidebar() {
+    const [like, setLike] = useState(false);
+
+    const handleClick = () => {
+        setLike(!like)
+    }
     return (
         <div className="sidebar">
+            <div className="sidebar__btn" onClick={handleClick}>
+                { !like ? 
+                    <FavoriteBorderIcon fontSize="large" />
+                    :
+                    <FavoriteIcon fontSize="large" className="sidebar__btn--liked"/>
+                }
+                
+                <p>200</p>
+            </div>
+            <div className="sidebar__btn">
+                <MessageIcon fontSize="large" />
+                <p>200</p>
+            </div>
+            <div className="sidebar__btn">
+                <ShareIcon fontSize="large" />
+                <p>200</p>
+            </div>
             
         </div>
     )
